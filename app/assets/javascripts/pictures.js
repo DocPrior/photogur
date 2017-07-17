d03d64217f68bcb9a06e3a29fc232d6d871d04e6
DocPrior <Sara Prior>
2017-07-17 13:20:27 -0400

    increment decount when activate card

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -4,6 +4,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   var cardList = document.querySelectorAll('.card');
   var activeButton = document.querySelectorAll('.active-state-trigger.button.button-primary');
+    var count = 0;
   for (var i = 0; i < activeButton.length; i++){
     var button = activeButton[i];
     button.addEventListener('click', function(e) {
@@ -13,13 +14,24 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
 
-      // var countDeck = document.querySelector('.deck-count');
-      //   var count = 0;
-      // function upCount() {
-      //     count += 1
-      //     return count
-      // }
-      // countDeck.innerHTML = upCount();
+
+
+  function upCount() {
+      count += 1
+      return count
+  }
+
+
+  for (var x = 0; x < activeButton.length; x++) {
+    var button = activeButton[x];
+    button.addEventListener('click', function(e) {
+        e.preventDefault();
+        var countDeck = document.querySelector('#deckCount');
+        countDeck.innerHTML = upCount()
+    })
+  }
+
+
 
 
 })
