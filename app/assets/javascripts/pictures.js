3654a861bbda238f18e7621c71255afdb074d3c1
DocPrior <Sara Prior>
2017-07-17 10:41:08 -0400

    able to add active class to cards

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -1,3 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
+document.addEventListener('DOMContentLoaded', function() {
+  var activeButton = document.querySelector('.active-state-trigger.button.button-primary');
+  activeButton.addEventListener('click', function(e) {
+      e.preventDefault();
+      var activeCard= document.querySelector('.card');
+      activeCard.setAttribute('class', 'active');
+    })
+})
