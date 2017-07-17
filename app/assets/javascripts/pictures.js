8e77eac2d2f771442d3524c86ca66fffcca3aab4
DocPrior <Sara Prior>
2017-07-17 12:21:26 -0400

    able to properly activate correct cards

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -2,10 +2,24 @@
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
 document.addEventListener('DOMContentLoaded', function() {
-  var activeButton = document.querySelector('.active-state-trigger.button.button-primary');
-  activeButton.addEventListener('click', function(e) {
-      e.preventDefault();
-      var activeCard= document.querySelector('.card');
-      activeCard.setAttribute('class', 'active');
+  var cardList = document.querySelectorAll('.card');
+  var activeButton = document.querySelectorAll('.active-state-trigger.button.button-primary');
+  for (var i = 0; i < activeButton.length; i++){
+    var button = activeButton[i];
+    button.addEventListener('click', function(e) {
+        e.preventDefault();
+        this.parentElement.setAttribute('class', 'active');
     })
+  }
+
+
+      // var countDeck = document.querySelector('.deck-count');
+      //   var count = 0;
+      // function upCount() {
+      //     count += 1
+      //     return count
+      // }
+      // countDeck.innerHTML = upCount();
+
+
 })
